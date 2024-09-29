157eac81d56d8ea4c27e54408d8dd76a2b448227
Rafael Bombonatti <bombonatti@gmail.com>
2024-09-29 19:22:41 -0300

    Muda para HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,11 +8,8 @@ use App\Models\News;
 use Inertia\Inertia;
 use Inertia\Response;
 
-class NewsController extends Controller
+class HomeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index(): Response
     {
         $newsList = News::with('category', 'tags')->get();
@@ -45,51 +42,5 @@ class NewsController extends Controller
             'search' => $search
         ]);
     }
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreNewsRequest $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(News $news)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(News $news)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateNewsRequest $request, News $news)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(News $news)
-    {
-        //
-    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,21 @@
 <?php
 
-use App\Http\Controllers\NewsController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', [NewsController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 
-Route::get('/news/{news}', [NewsController::class, 'details'])->name('news.details');
-Route::get('/search/{search}', [NewsController::class, 'search'])->name('news.search');
+Route::get('/news/{news}', [HomeController::class, 'details'])->name('news.details');
+Route::get('/search/{search}', [HomeController::class, 'search'])->name('news.search');
 
-//Route::get('/dashboard', function () {
-//    return Inertia::render('Dashboard');
-//})->middleware(['auth', 'verified'])->name('dashboard');
-//
-//Route::middleware('auth')->group(function () {
-//    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-//});
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 require __DIR__.'/auth.php';
